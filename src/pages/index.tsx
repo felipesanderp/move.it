601e9d398541b9b6920c71603cdf79bcb1431b40
Felipe Sander <felipe.panisson@hotmail.com>
2022-07-29 13:57:34 -0300

    add favicon and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "../../node_modules/next/head";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -8,6 +9,10 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
