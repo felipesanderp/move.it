81774d38d468e47a94045b4f59caeb02f0925fa0
Felipe Sander <felipe.panisson@hotmail.com>
2022-07-29 11:45:21 -0300

    create profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/felipesanderp.png" alt="Felipe Sander" />
+      <div>
+        <strong>Felipe Sander</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
