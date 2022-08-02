f73fc165d06273b26cc8684f1ae0f1bf79f33e50
Felipe Sander <felipe.panisson@hotmail.com>
2022-08-02 17:35:10 -0300

    create signin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { Envelope, GithubLogo, GoogleLogo } from 'phosphor-react'
+import { Logo } from '../components/Logo';
+
+import styles from '../styles/pages/SignIn.module.css';
+
 export default function SigIn() {
   return (
-    <h1>SigIn</h1>
+    <div className={styles.signInContainer}>
+      <div className={styles.content}>
+        <div />
+        <aside>
+          <div className={styles.loginForm}>
+            <Logo />
+
+            <span>Bem-vindo</span>
+            <p>Faça login com seu E-mail, GitHub ou Google para começar</p>
+            <div className={styles.icons}>
+              <Envelope size={30} />
+              <GithubLogo size={30} />
+              <GoogleLogo size={30} />
+            </div>
+          </div>
+        </aside>
+      </div>
+    </div>
   )
 }
