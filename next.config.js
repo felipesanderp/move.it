3244c125007375547e20edfc22d924a8a836e46e
Felipe Sander <felipe.panisson@hotmail.com>
2022-08-02 11:39:03 -0300

    remove pwa config

--- next.config.js
+++ next.config.js
@@ -5,14 +5,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
